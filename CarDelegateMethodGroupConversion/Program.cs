50f470bca3a1ad4c4c83b47ac170a111e6b89f07
sanda15 <itss07@gmail.com>
2018-05-26 12:25:51 -0400

    not directly allocating assocated delegate obj, butroath simpley specifying a method that matches the delegate's expeted signature

---
+++ CarDelegateMethodGroupConversion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarDelegateMethodGroupConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Method Group Conversion *****\n");
+            // note we are not directly allocatin the associated delegate obj, but rather simply specifying a method that matatch
+            // the delegate's expected signature.
+            Car c1 = new Car();
+            // old way of register
+            //c1.RegisterWithCarEngine(new Car.CarEngineHandler(CallMeHere));
+            
+            // Register the simple method name.
+           
+             c1.RegisterWithCarEngine(CallMeHere);
+
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            // Unregister the simple method name.
+           c1.UnRegisterWithCarEngine(CallMeHere);
+
+            // No more notifications!
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            Console.ReadLine();
+        }
+
+        static void CallMeHere(string msg)
+        {
+            Console.WriteLine("=> Message from Car: {0}", msg);
+        }
+    }
+}
