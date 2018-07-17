472da8601dcac068c3752b15aa90f2d02fe0e6d9
sanda15 <itss07@gmail.com>
2018-07-17 06:35:16 -0400

    Update BatterySOC.h

--- 2016/soc_sun/BatterySOC.h
+++ 2016/soc_sun/BatterySOC.h
@@ -1,37 +1,5 @@
 #pragma once
-/*******************************************************************************
-*
-* Copyright (c) 2016 Revision Military Incorporated. All Rights Reserved.
-*
-* This software is protected by copyright law and international treaties.
-* The use, copying, modification, and distribution of this software and
-* its documentation, in any form, is strictly prohibited, except with the
-* express written permission or licence of Revision Military Incorporated.
-* Failure to abide by the terms of this Notice may result in severe
-* criminal and civil liabilities.
-*
-* THIS SOFTWARE IS BEING PROVIDED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
-* WARRANTY. IN PARTICULAR, REVISION MILITARY INCORPORATED MAKES NO
-* REPRESENTATION OR WARRANTY OF ANY KIND CONCERNING THE MERCHANTABILITY
-* OF THIS SOFTWARE OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
-*
-******************************************************************************/
 
-
-/*******************************************************************************
-*
-*  FILENAME:     BatterySOC.h
-*
-*  DESCRIPTION:  The target-specific data sizes
-*
-*
-*  $Rev$
-*  $Date$
-*  $Author$
-*  $HeadURL$
-*
-*
-******************************************************************************/
 #ifndef __INCLUDE_types_
 #define __INCLUDE_types_
 #endif
