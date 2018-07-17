825ee23feed5c56e3b4ea90c30063ce844cf4fc8
Lisa Shi <lshi@revisionmiltary.com>
2018-07-17 16:33:16 -0400

    Simplified table stat do interperlate  but 50C data wrong.

---
+++ 2016/soc_tues/SOC/BatteryUser.h
@@ -0,0 +1,73 @@
+// BatteryUser.h
+#pragma once
+/*******************************************************************************
+*
+* Copyright (c) 2016 Revision Military Incorporated. All Rights Reserved.
+*
+* This software is protected by copyright law and international treaties.
+* The use, copying, modification, and distribution of this software and
+* its documentation, in any form, is strictly prohibited, except with the
+* express written permission or licence of Revision Military Incorporated.
+* Failure to abide by the terms of this Notice may result in severe
+* criminal and civil liabilities.
+*
+* THIS SOFTWARE IS BEING PROVIDED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
+* WARRANTY. IN PARTICULAR, REVISION MILITARY INCORPORATED MAKES NO
+* REPRESENTATION OR WARRANTY OF ANY KIND CONCERNING THE MERCHANTABILITY
+* OF THIS SOFTWARE OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
+*
+******************************************************************************/
+
+
+/*******************************************************************************
+*
+*  FILENAME:     BatteryUser.h
+*
+*  DESCRIPTION:  The target-specific data sizes
+*
+*
+*  $Rev$
+*  $Date$
+*  $Author$
+*  $HeadURL$
+*
+*
+******************************************************************************/
+#ifndef __INCLUDE_types_
+#define __INCLUDE_types_
+#endif
+
+
+//#define Tmin (int8_t)(-40) 
+//#define Tmax (int8_t)55
+#define Tmin (int8_t)(25) 
+#define Tmax (int8_t)55
+//#define Tmin (int8_t)(-50) 
+//#define Tmax (int8_t)90
+
+
+//#define cellVmin (uint16_t)2200
+//#define cellVmax (uint16_t)5000
+
+#define SOChigh (uint8_t)(90)
+#define SOClow   (uint8_t)(10)
+//#define cellVmin (uint16_t)2200
+//#define cellVmax (uint16_t)4300
+#define cellVmin (uint16_t)1000
+#define cellVmax (uint16_t)4000
+
+#define cellVTblMax (uint16_t)1000
+#define cellVTblMin  (uint16_t)4000
+
+#define newChemCols (uint8_t)4
+//#define stepSOC  (uint8_t)5
+#define stepSOC  (uint8_t)20
+//#define deltaIdxSOC (uint8_t)2  // Where 90%, 10% located in i+2 or sizeof(Array) -2
+//#define newChemRows (uint8_t)21
+#define newChemRows (uint8_t)6
+
+
+
+
+
+
---
+++ 2016/soc_tues/SOC/main.c
@@ -0,0 +1,131 @@
+//main.c
+
+#ifndef __INCLUDE_types_
+#define __INCLUDE_types_
+#endif
+#include <stdint.h>
+#include "BatterySOC.h"
+#include <stdio.h>
+
+
+
+uint8_t str[100];
+//extern void prepare();
+	  uint8_t soc = 0;
+	 // Create test cases for temperature between Min to Max at range of Cell voltage
+     // between VcellMax and VcellMin 
+     int8_t temperature = Tmin ; 
+      
+	  uint16_t cellV = cellVmin; 
+	  uint8_t ret; // 
+      
+      
+      uint8_t rowV = 200;
+      uint8_t rowT = 200;
+      uint8_t countV = 0;
+      uint8_t countT = 0;
+
+    int main(int argc, char **argv)
+    
+	{ 
+            printf(" %d \n" ,temperature);
+           FILE *fp;          
+             fp = fopen("soc.csv", "w+c");
+                        
+             if(fp == NULL)
+            {
+             printf ("\n fail to open file");
+             return 1;                
+            }
+
+			/* for ( uint8_t k = 0; k < 10; k++)
+			 {
+				 fwrite(str,  1, sizeof(str), fp);
+				 printf("Saving...\nSaved.");
+			 }*/
+
+
+      // prepare the row 1 for all temerature ranges
+            
+    //prepare();  //soc1.csv prepare
+      
+      // str[i] = getSOC(2500, -40,0);
+			 cellV = cellVmin;
+   
+   int data , rc;
+   for (uint8_t i = 0; i < 10; i++)
+   {
+	   str[i] = getSOC(cellV, 25, 0);
+	  
+	   cellV = cellV + 200;
+	   printf("str[i]= %d, \n", str[i]);
+	   /*if (fputs(str, fp) != EOF)
+	   {
+		   rc = 1;
+	   }*/
+
+   }
+
+    
+
+   //fclose(fp); // or for the paranoid: if (fclose (fOut) == EOF) rc = 0;
+	  //   
+   //scanf("%d", &data);
+   //   
+   //return ret;
+   //    }
+
+     
+   
+      fprintf(fp, "%d,", 9999);  // for place holder
+     
+       
+      for( uint8_t idx =  0;  idx < 200 ; idx ++)
+         {
+             uint16_t rowOne = idx * stepV + cellVmin;
+               if  (( rowOne  >= (uint16_t)cellVmin)  && (rowOne  <= (uint16_t)cellVmax))
+               {                  
+				   countV++;
+                    fprintf(fp, "%d,", rowOne);
+               }
+         }
+          
+        // GET SOC
+        for ( uint8_t j = 0; j < rowT ; j++)      
+            { 
+            printf("in top level For \n");
+            printf("temp = %d, \n", temperature);
+            
+          //  fprintf(fp," @Temperature of %d \n", temperature); 
+            fprintf(fp,"\n %d, ", temperature);
+            
+            cellV = cellVmin;  // reset cellVmin for next iteration
+            
+              if ((temperature < (int8_t)(Tmin)) || (temperature > (int8_t)(Tmax) ))  // done  
+           {
+        printf ( "out of temperature range \n");
+         return 0;
+         }
+			  temperature = j == 0 ? temperature :  temperature + stepT;
+                // do once at least for start temperature point
+                                            
+                   for (uint8_t i = 0 ; i < rowV; i ++)
+               {
+                if  ((cellV < (uint16_t)cellVmin)  || (cellV > (uint16_t)cellVmax))
+                       {
+                      break;
+                    }  
+                    ret = getSOC(cellV,temperature, soc);              
+                 
+                  fprintf(fp,"%03d, ", ret);                 
+                  cellV = cellV + stepV ;
+                }                                                            
+            }   
+                          
+            fclose(fp);
+             return 0;   
+    } 
+            
+           
+        
+		
